1c08b43703ec93f8ed466958c7bcd34588e7e5d4
Leandro Braz Alves <alves_leandro@hotmail.com>
2022-08-24 10:34:53 -0300

    Curso Certificação Java

--- PastaDiretorio/Roberta.java
+++ PastaDiretorio/Roberta.java
@@ -1,22 +1,32 @@
 package PastaDiretorio;
 
-import PastaDiretorio.Dao.Dao;
-import PastaDiretorio.Modelo.Modelo;
 
-class Amor{
+
+class Calculadora{
+
+    public int calcula(int... numeros){
+
+        int soma = 0;
+
+        for (int i = 0; i < numeros.length; i++){
+
+            soma += numeros[i];
+
+        }
+        return soma;
+
+    }
 
 }
 
 public class Roberta {
     public static void main(String[] args) {
 
-        int [] repeticao = new int[20];
-        Amor amor;
-        Modelo modelo = null;
-        Dao dao;
-        ClasseImporta classeImporta;
+        Calculadora c = new Calculadora();
 
+        System.out.println(c.calcula(1,2));
+        System.out.println(c.calcula(1,2,3,4,5,6,7,8,9));
+        System.out.println(c.calcula(100,200,39999,5678));
 
-        System.out.println(repeticao[1]);
     }
 }
